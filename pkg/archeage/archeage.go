4ea39f697ac333d6d8772cb44c557fa2cf309bd8
DevMyong <myong.dev@gmail.com>
2021-11-03 17:15:28 +0900

    Add: base code for http

---
+++ pkg/archeage/archeage.go
@@ -0,0 +1,50 @@
+package archeage
+
+import (
+	"archecord/pkg/user"
+	"github.com/PuerkitoBio/goquery"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type Archeage struct {
+	client *http.Client
+}
+
+func New(c *http.Client) *Archeage {
+	return &Archeage{c}
+}
+
+func (a *Archeage) Get(url string) (*goquery.Document, error) {
+	return a.do("GET", url, nil)
+}
+
+func (a *Archeage) post(url string, body io.Reader) (*goquery.Document, error) {
+	return a.do("POST", url, body)
+}
+
+func (a *Archeage) do(method, url string, body io.Reader) (*goquery.Document, error) {
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+
+	resp, err := a.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return doc, err
+}
+
+func SetURI(host string, path string, params *url.Values) url.URL {
+	return url.URL{Scheme: user.SchemeDefault, Host: host, Path: path, RawQuery: params.Encode()}
+}
---
+++ pkg/archeage/common.go
@@ -0,0 +1,48 @@
+package archeage
+
+import (
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+)
+
+const (
+	Token    string = ""
+	BasicURL string = "https://archeage.xlgames.com"
+
+	West = 0
+	East = 1
+
+	SearchMaterialName = "table:nth-child(1) > tbody > tr:nth-child(%d) > th"
+	SearchWestRegion   = "table:nth-child(1) > tbody > tr:nth-child(%d) > td > ul > li.point"
+	SearchEastRegion   = "table.table-bond.right > tbody > tr:nth-child(%d) > td > ul > li.point"
+)
+
+var ServerNameMap = map[string]string{
+	"누이":    	"NUI",
+	"하제":    	"HAJE",
+	"다후타": 	"DAHUTA",
+	"모르페우스":	"MORPHEUS",
+	"랑그레이": 	"RANGORA",
+	"환락":    	"SEASON",
+}
+
+func BasicParser(url string) (doc *goquery.Document) {
+	resp, err := http.Get(BasicURL + url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("StatusCode Error: %d %s", resp.StatusCode, resp.Status)
+	}
+
+	// Load HTML
+	doc, err = goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return
+}
